e38bee294362f76abc739919f33ed3a40c7f4940
SPURGE <apple@Apples-MacBook-Pro.local>
2022-11-25 07:39:27 +0530

    updated

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import "./Card.css"
 export default function ({contents}) {
   return (
-    <div>
-      <div>image</div>
-      <div>
-        {contents}
+    <div className="card-contr">
+      <div className="card-image">
+        <img className="card-logo" src={require("../../Assets/Images/Moo-Beyond-Logo-Final 1.png")}/>
+      </div>
+      <div className="card-contents">
+       <p className="card-text"> {contents}</p>
       </div>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -61,7 +61,7 @@ export default function Home() {
       <button className="view-more-btn">view more</button>
       <div className="add-banner-cntr">
         <div className="banner-lcontr">
-          <h2>THE MOO BEYOND EDGE/LET'S MOO BEYOND</h2>
+          <h2>THE MOO BEYOND EDGE</h2>
           <p>
             What are the characteristic markers of genuine organic A2 ghee? And
             how can you verify its authenticity? Let's check the facts, and
@@ -72,30 +72,99 @@ export default function Home() {
             a mile ahead other products by being the first A2 ghee to have more
             than 7 certifications
           </p>
-          <p>
-            Second, is the product itself. Our A2 ghee surpasses any other in
-            terms of quality, taste and consistency. the deep colour of golden
-            dandelion yellow, the rich aroma of nutty earthiness, the grainy
-            semi-liquid texture, and last but not the least is the taste which
-            is an exploration into all five of the senses, a final testimony to
-            the highest quality of standards we abide to. With Moo Beyond's 100%
-            organic A2 ghee, you will finally discover the essential superiority
-            of natural and authentic ghee, and realize that you have been
-            missing out.
-          </p>
+          <button className="order-now-btn">ORDER NOW</button>
         </div>
         <div className="banner-rcontr">
-          <div>image1</div>
-          <div>
-            <div>image 2</div>
-            <div>image 3</div>
+          <div className="banner-image">
+            <img
+              className="banner-points-image"
+              src={require("../Assets/Images/young-male.png")}
+            />
+          </div>
+
+          <div className="image-botm-contr">
+            <div className="sub-banner-image">
+              <img
+                className="motherdaughter"
+                src={require("../Assets/Images/mother-doughter.png")}
+              />
+            </div>
+            <div className="sub-banner-image">
+              <img
+                className="manwithghee"
+                src={require("../Assets/Images/man-with-ghee.png")}
+              />
+            </div>
           </div>
         </div>
       </div>
-
+      <h2>OUR PRODUCT</h2>
       <div className="product-contr">
-        <div className="product-info">main image</div>
-        <div className="product-descr">main image</div>
+        <div className="product-info">
+          <div className="prd-left-contr">
+            <img
+              className="ghee-prd-main"
+              src={require("../Assets/Images/gee-box.png")}
+            />
+          </div>
+
+          <div className="prd-right-contr">
+            <img
+              className="bill-image"
+              src={require("../Assets/Images/gee-box.png")}
+            />
+            <img
+              className="bill-image"
+              src={require("../Assets/Images/gee-box.png")}
+            />
+          </div>
+        </div>
+        <div className="product-descr">
+          <h3>ORGANIC A2 COW GHEE</h3>
+          <p>CLARIFIED BUTTER</p>
+          <p>$1200</p>
+          <div className="product-action-contr">
+            <div  className="product-counter"><span className="action">-</span><span className="action" >0</span><span className="action">+</span ></div >
+            <button  className="product-cart-btn">Add to cart</button>
+          </div>
+          <div>
+            <p className="point">
+              1.Rich in antioxidants and vital vitamins like B2. B12. B6. C. E.
+              K and fattv acids like Omega-3. Omega-6 as well as healthy amino
+              acids.
+            </p>
+            <p className="point">
+              2.Moo Beyond organic A ghee can nourish the body, Improve brain
+              tunction, hone health increase immunity, strength en the and
+              enhance digestion and imorove liver neatn.
+            </p>
+            <p className="point">
+              3.Great source of nutrition for pregnant women, under-nourished
+              children and adolescents, health tood enthusiasts, diabetics and
+              people with heart disease and dietary restrictions as well as Keto
+              Friendly.
+            </p>
+            <p className="point">
+              4.Cosmetic adolications like moisturising the skin. babv massage
+              oil replacement, reduce dark circles around and eves and helps
+              reiuvenate chapped lips.
+            </p>
+            <p className="point">
+              5. 100% A2 protein from ayurvedically trusted Man cows. whose milk
+              is rich in medicinal properties and increases resistance to
+              several diseases.
+            </p>
+            <p className="point">
+              5. 100% A2 protein from ayurvedically trusted Man cows. whose milk
+              is rich in medicinal properties and increases resistance to
+              several diseases.
+            </p>
+            <p className="point">
+              5. 100% A2 protein from ayurvedically trusted Moo Bevond A2 ghee
+              comes with a shelf- ife of 1 veal.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
